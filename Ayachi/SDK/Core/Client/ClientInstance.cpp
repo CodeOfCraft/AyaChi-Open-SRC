37d10920d9eec7e73d1f1975cf08bc7517fe988e
Yuki <coolyu123@126.com>
2026-09-26 01:33:14 +0800

    feat(sdk): expand ClientInstance interfaces

--- Ayachi/SDK/Core/Client/ClientInstance.cpp
+++ Ayachi/SDK/Core/Client/ClientInstance.cpp
@@ -19,6 +19,14 @@ ClientInstance* ClientInstance::get() {
 	return instance;
 }
 
+void ClientInstance::reset() {
+	instance = nullptr;
+}
+
+void ClientInstance::tick() {
+	MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::tick, void>(this);
+}
+
 bool ClientInstance::update(bool isInitFinished) {
 	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::update, bool, bool>(this, isInitFinished);
 }
@@ -31,12 +39,164 @@ LocalPlayer* ClientInstance::getLocalPlayer() {
 	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getLocalPlayer, LocalPlayer*>(this);
 }
 
-bool ClientInstance::getMouseGrabbed() {
-	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getMouseGrabbed, bool>(this);
+Actor* ClientInstance::getCameraActor() {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getCameraActor, Actor*>(this);
+}
+
+Actor* ClientInstance::getCameraTargetActor() {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getCameraTargetActor, Actor*>(this);
+}
+
+Minecraft* ClientInstance::getServerData() {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getServerData, Minecraft*>(this);
+}
+
+bool ClientInstance::isLeavingGame() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isLeavingGame, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isDestroyingGame() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isDestroyingGame, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isShuttingDown() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isShuttingDown, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isSplitScreenActive() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isSplitScreenActive, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isExitingLevel() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isExitingLevel, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isInBedScreen() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isInBedScreen, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isInDeathScreen() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isInDeathScreen, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isKeyboardEnabled() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isKeyboardEnabled, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::hasCommands() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::hasCommands, bool>(const_cast<ClientInstance*>(this));
+}
+
+int ClientInstance::getSplitScreenCount() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getSplitScreenCount, int>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isShowingLoadingScreen() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isShowingLoadingScreen, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isReadyToRender() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isReadyToRender, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::useController() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::useController, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::useTouchscreen() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::useTouchscreen, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::getMouseGrabbed() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getMouseGrabbed, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::currentInputModeIsMouseAndKeyboard() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::currentInputModeIsMouseAndKeyboard, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::allowPicking() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::allowPicking, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isShowingMenu() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isShowingMenu, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isShowingPauseScreen() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isShowingPauseScreen, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isShowingProgressScreen() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isShowingProgressScreen, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isShowingWorldProgressScreen() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isShowingWorldProgressScreen, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isShowingRealmsProgressScreen() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isShowingRealmsProgressScreen, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isShowingDeathScreen() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isShowingDeathScreen, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isShowingServerForm() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isShowingServerForm, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isScreenReplaceable() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isScreenReplaceable, bool>(const_cast<ClientInstance*>(this));
 }
 
 Level* ClientInstance::getLevel() {
 	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getLevel, Level*>(this);
+}
+
+bool ClientInstance::isWorldActive() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isWorldActive, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isInRealm() {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isInRealm, bool>(this);
+}
+
+bool ClientInstance::readyForShutdown() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::readyForShutdown, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isPrimaryClient() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isPrimaryClient, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isEduMode() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isEduMode, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isGamepadCursorEnabled() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isGamepadCursorEnabled, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isInControlCustomization() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isInControlCustomization, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::hasLevel() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::hasLevel, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isPreGame() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isPreGame, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isInMultiplayerGame() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isInMultiplayerGame, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isMultiPlayerClient() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isMultiPlayerClient, bool>(const_cast<ClientInstance*>(this));
 }
 
 bool ClientInstance::isInWorldAndNotShowingAnyMenuScreens() const {
@@ -51,10 +211,10 @@ MinecraftGame* ClientInstance::getMinecraftGame() {
 		MinecraftGame*>(this);
 }
 
-IOptionRegistry* ClientInstance::getOptions() {
+void* ClientInstance::getOptions() {
 	return MemoryUtil::CallVFunc<
 		GameData::VTableIndex::ClientInstance::getOptions,
-		IOptionRegistry*>(this);
+		void*>(this);
 }
 
 GameRenderer* ClientInstance::getGameRenderer() {
@@ -65,16 +225,77 @@ LevelRenderer* ClientInstance::getLevelRenderer() {
 	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getLevelRenderer, LevelRenderer*>(this);
 }
 
+bool ClientInstance::shouldRenderUICursor() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::shouldRenderUICursor, bool>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::getRenderPlayerModel() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getRenderPlayerModel, bool>(const_cast<ClientInstance*>(this));
+}
+
+float ClientInstance::getGuiScale() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getGuiScale, float>(const_cast<ClientInstance*>(this));
+}
+
 GuiData* ClientInstance::getGuiData() {
-	return *reinterpret_cast<GuiData**>(reinterpret_cast<uintptr_t>(this) + 0x648);
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getGuiData, GuiData*>(this);
 }
 
 PacketSender* ClientInstance::getPacketSender() {
 	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getPacketSender, PacketSender*>(this);
 }
 
-ClientInputHandler* ClientInstance::getInput() {
-	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getInput, ClientInputHandler*>(this);
+void* ClientInstance::getInput() {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getInput, void*>(this);
+}
+
+int ClientInstance::getCurrentInputMode() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getCurrentInputMode, int>(const_cast<ClientInstance*>(this));
+}
+
+void* ClientInstance::getMinecraftInput() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getMinecraftInput, void*>(const_cast<ClientInstance*>(this));
+}
+
+Vec3<float> ClientInstance::getLastPointerLocation() {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getLastPointerLocation, Vec3<float>>(this);
+}
+
+Vec2<float> ClientInstance::getTouchPointerLocation(int id) const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getTouchPointerLocation, Vec2<float>, int>(
+		const_cast<ClientInstance*>(this), id);
+}
+
+int ClientInstance::getActionPointerId() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getActionPointerId, int>(const_cast<ClientInstance*>(this));
+}
+
+int ClientInstance::getControllerId() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getControllerId, int>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::hasConnectedController() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::hasConnectedController, bool>(const_cast<ClientInstance*>(this));
+}
+
+void ClientInstance::refocusMouse(bool lostMouse) {
+	MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::refocusMouse, void, bool>(this, lostMouse);
+}
+
+void* ClientInstance::getSoundEngine() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getSoundEngine, void*>(const_cast<ClientInstance*>(this));
+}
+
+void* ClientInstance::getMusicManager() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getMusicManager, void*>(const_cast<ClientInstance*>(this));
+}
+
+bool ClientInstance::isFullVanillaPackOnStack() const {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::isFullVanillaPackOnStack, bool>(const_cast<ClientInstance*>(this));
+}
+
+ItemInHandRenderer* ClientInstance::getItemInHandRenderer() {
+	return MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::getItemInHandRenderer, ItemInHandRenderer*>(this);
 }
 
 ItemRenderer* ClientInstance::getItemRenderer() {
@@ -83,14 +304,26 @@ ItemRenderer* ClientInstance::getItemRenderer() {
 		ItemRenderer*>(this);
 }
 
+void ClientInstance::grabMouse() {
+	MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::grabMouse, void>(this);
+}
+
+void ClientInstance::releaseMouse() {
+	MemoryUtil::CallVFunc<GameData::VTableIndex::ClientInstance::releaseMouse, void>(this);
+}
+
 void ClientInstance::grabCursor() {
-	if (Addresses::ClientInstance_grabCursor == 0)
+	if (Addresses::ClientInstance_grabCursor != 0) {
+		reinterpret_cast<void(__fastcall*)(void*)>(Addresses::ClientInstance_grabCursor)(this);
 		return;
-	reinterpret_cast<void(__fastcall*)(void*)>(Addresses::ClientInstance_grabCursor)(this);
+	}
+	grabMouse();
 }
 
 void ClientInstance::releaseCursor() {
-	if (Addresses::ClientInstance_releaseCursor == 0)
+	if (Addresses::ClientInstance_releaseCursor != 0) {
+		reinterpret_cast<void(__fastcall*)(void*)>(Addresses::ClientInstance_releaseCursor)(this);
 		return;
-	reinterpret_cast<void(__fastcall*)(void*)>(Addresses::ClientInstance_releaseCursor)(this);
+	}
+	releaseMouse();
 }
--- Ayachi/SDK/Core/Client/ClientInstance.h
+++ Ayachi/SDK/Core/Client/ClientInstance.h
@@ -2,18 +2,17 @@
 #include <string>
 #include "../../../Utils/Maths.h"
 #include "../../../Utils/MemoryUtil.h"
+#include "../../Network/PacketSender.h"
+#include "../../Render/GameRenderer.h"
+#include "../../Render/ItemInHandRenderer.h"
+#include "../../Render/ItemRenderer.h"
+#include "../../Render/LevelRenderer.h"
+#include "../../World/Actor/Actor.h"
+#include "../../World/Actor/Player/LocalPlayer.h"
 #include "../../World/Level/BlockSource.h"
-
-class Minecraft;
-class MinecraftGame;
-class LevelRenderer;
-class PacketSender;
-class ClientInputHandler;
-class LocalPlayer;
-class Level;
-class IOptionRegistry;
-class GameRenderer;
-class ItemRenderer;
+#include "../../World/Level/Level.h"
+#include "../../World/Level/Minecraft.h"
+#include "MinecraftGame.h"
 
 #include "../GUI/GuiData.h"
 
@@ -23,26 +22,85 @@ public:
 	CLASS_MEMBER(Minecraft*, minecraft, 0x1A8);
 	CLASS_MEMBER(LevelRenderer*, levelRenderer, 0x1B8);
 	CLASS_MEMBER(PacketSender*, packetSender, 0x1C8);
-	CLASS_MEMBER(ClientInputHandler*, inputHandler, 0x1D8);
+	CLASS_MEMBER(void*, inputHandler, 0x1D8);
 	CLASS_MEMBER(Vec2<float>, cursorPos, 0x580);
 
 	static ClientInstance* get();
+	static void reset();
 
+	void tick();
 	bool update(bool isInitFinished);
 	BlockSource* getRegion();
 	LocalPlayer* getLocalPlayer();
-	bool getMouseGrabbed();
+	Actor* getCameraActor();
+	Actor* getCameraTargetActor();
+	Minecraft* getServerData();
+
+	bool isLeavingGame() const;
+	bool isDestroyingGame() const;
+	bool isShuttingDown() const;
+	bool isSplitScreenActive() const;
+	bool isExitingLevel() const;
+	bool isInBedScreen() const;
+	bool isInDeathScreen() const;
+	bool isKeyboardEnabled() const;
+	bool hasCommands() const;
+	int getSplitScreenCount() const;
+	bool isShowingLoadingScreen() const;
+	bool isReadyToRender() const;
+
+	bool useController() const;
+	bool useTouchscreen() const;
+	bool getMouseGrabbed() const;
+	bool currentInputModeIsMouseAndKeyboard() const;
+	bool allowPicking() const;
+	bool isShowingMenu() const;
+	bool isShowingPauseScreen() const;
+	bool isShowingProgressScreen() const;
+	bool isShowingWorldProgressScreen() const;
+	bool isShowingRealmsProgressScreen() const;
+	bool isShowingDeathScreen() const;
+	bool isShowingServerForm() const;
+	bool isScreenReplaceable() const;
 	Level* getLevel();
+	bool isWorldActive() const;
+	bool isInRealm();
+	bool readyForShutdown() const;
+	bool isPrimaryClient() const;
+	bool isEduMode() const;
+	bool isGamepadCursorEnabled() const;
+	bool isInControlCustomization() const;
+	bool hasLevel() const;
+	bool isPreGame() const;
+	bool isInMultiplayerGame() const;
+	bool isMultiPlayerClient() const;
 	bool isInWorldAndNotShowingAnyMenuScreens() const;
 	MinecraftGame* getMinecraftGame();
-	IOptionRegistry* getOptions();
+	void* getOptions();
 	GameRenderer* getGameRenderer();
 	LevelRenderer* getLevelRenderer();
+	bool shouldRenderUICursor() const;
+	bool getRenderPlayerModel() const;
+	float getGuiScale() const;
 	GuiData* getGuiData();
 	PacketSender* getPacketSender();
-	ClientInputHandler* getInput();
+	void* getInput();
+	int getCurrentInputMode() const;
+	void* getMinecraftInput() const;
+	Vec3<float> getLastPointerLocation();
+	Vec2<float> getTouchPointerLocation(int id) const;
+	int getActionPointerId() const;
+	int getControllerId() const;
+	bool hasConnectedController() const;
+	void refocusMouse(bool lostMouse);
+	void* getSoundEngine() const;
+	void* getMusicManager() const;
+	bool isFullVanillaPackOnStack() const;
+	ItemInHandRenderer* getItemInHandRenderer();
 	ItemRenderer* getItemRenderer();
 
+	void grabMouse();
+	void releaseMouse();
 	void grabCursor();
 	void releaseCursor();
 
